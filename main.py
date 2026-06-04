e98cad1518be10d06de150babf19e5c7d3e13cc4
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2026-06-05 01:14:25 +0530

    Data Transformation completed

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation_obj = DataTransformation()
+    data_transformation_obj.initiate_data_transformation(train_data, test_data)
